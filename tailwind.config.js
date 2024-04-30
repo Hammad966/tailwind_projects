c5e165c59c9b6d0342bafe3b6464422956073be9
hammad_ahmad <h.hammad2066@gmail.com>
2024-04-30 10:59:55 +0500

    My successfull js projects

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['Roboto', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
